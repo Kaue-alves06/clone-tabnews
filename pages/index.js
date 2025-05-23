276e830506afaba738af7ad8af53a69410958809
Kaue-alves06 <kauecoelh@gmail.com>
2025-05-23 22:50:44 +0000

    adiciona scripts `lint:check` e `lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Minha princesa se cuida e vê se melhora, pra eu poder te ver e te abraçarkkkk🧡🧡</h1>
+  return (
+    <h1>
+      Minha princesa se cuida e vê se melhora, pra eu poder te ver e te
+      abraçarkkkk🧡🧡
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
